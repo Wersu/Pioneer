e37244337c74fecc90834eec400d1a1bcca8a176
Мария Мелентьева <masha696nik@gmail.com>
2025-03-14 21:23:00 +0500

    add React version

---
+++ data.js
@@ -0,0 +1,44 @@
+const productImgs = [{
+    'id':'0001',
+    'url': 'images/level_indicator.png',
+    'title': 'Сигнализатор уровня',
+},{
+    'id':'0002',
+    'url': 'images/title-aero.png',
+    'title': 'Датчики давления',
+},{
+    'id':'0003',
+    'url': 'images/kamaz.png',
+    'title': 'ПИД-регуляторы',
+},{
+    'id':'0004',
+    'url': 'images/turbo.png',
+    'title': 'Калибратор давления',
+},{
+    'id':'0005',
+    'url': 'images/main-slide-power.png',
+    'title': 'Корректор расхода газа',
+},{
+    'id':'0006',
+    'url': 'images/engine.png',
+    'title': 'Манометры',
+},
+]
+
+const news = [{
+    'id':'0007',
+    'url':'images/jonas-morgner.jpg',
+    'title':'НПП «ПИОНЕР» подтвердило свои компетенции в области консультирования и изготовления…',
+    'date':'20.07.2020',
+},{
+    'id':'0008',
+    'url':'images/greg-rosenke.png',
+    'title':'Поздравляем коллектив регионального представительства «ПИОНЕР» с праздником!',
+    'date':'18.07.2020',
+},{
+    'id':'0009',
+    'url':'images/rafael-juarez.png',
+    'title':'Перенос сроков проведения запланированных работ в 2020 году',
+    'date':'20.06.2020',
+},
+]
